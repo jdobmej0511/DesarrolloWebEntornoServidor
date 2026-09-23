f34872bd924ac1ebeb0e7a79d8a74c2d4085224d
José Carlos Doblas Mejías <jdobmej0511@cpifpalanturing.es>
2026-09-23 11:05:49 +0200

    Second Commit

--- src/cadenas.php
+++ src/cadenas.php
@@ -1,11 +1,34 @@
 <?php
     $valor = 15;
-    echo "El prefcio del la artículoes: {$valor}€<br/>";
-    echo 'El precio del la artículo es: $valor <br/>';
+    echo "El prefcio del artículoes: {$valor}€<br/>";
+    echo 'El precio del artículo es: $valor <br/>';
 
-    #Tipo Array
+    # Tipo Array
     $a = [6, -1, 3, 23, 8, 11] ;
-    print_r($a);
+    print_r($a) ;
 
-    echo "<br/>"
+    echo "<br/>" ;
+
+    unset($a[5]) ;
+    print_r($a) ;
+
+    echo "<br/>" ;
+
+    $a[] = 999 ;
+    print_r($a) ;
+
+    echo "<br/>";
+
+    $b = ["a" => 1, "d" => 5];
+    $c = ["a" => 2, 3, 6];
+    $d = ["c" => 4, ...$b, ...$c];
+
+    print_r($d);
+
+    echo "<br/>" ;
+
+    $resultado = "     hola, clase de dwes     " |> rtrim(...) |> strtoupper(...) ;
+
+    echo "<pre>" . print_r($resultado, true) . "</pre>" ;
+
 ?>
