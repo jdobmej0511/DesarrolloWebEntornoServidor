b22dd85424cccd284ab12c53011596c97c744e5f
José Carlos Doblas Mejías <jdobmej0511@cpifpalanturing.es>
2026-09-23 10:10:25 +0200

    First commit

---
+++ src/cadenas.php
@@ -0,0 +1,11 @@
+<?php
+    $valor = 15;
+    echo "El prefcio del la artículoes: {$valor}€<br/>";
+    echo 'El precio del la artículo es: $valor <br/>';
+
+    #Tipo Array
+    $a = [6, -1, 3, 23, 8, 11] ;
+    print_r($a);
+
+    echo "<br/>"
+?>
---
+++ src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <?php
+        $total = 0;
+        for ($i = 0; $i < 5; $i++) {
+            $total += $i;
+            echo $total;
+            echo "<br>";
+        }
+    
+        echo "¡Hola mundo!"; 
+        echo "<br>";
+        ?>
+        
+        <br>
+        cagon
+    </body>
+</html>
